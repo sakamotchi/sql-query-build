49fd93456a48ac0c6799670b05565dec969480e7
sakamotchi <31853314+sakamotchi@users.noreply.github.com>
2025-09-27 15:38:55 +0900

    [add]Vuetify追加

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light'
+  }
+})
+
+createApp(App).use(vuetify).mount("#app");
